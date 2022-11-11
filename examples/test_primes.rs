cc9fd27d1a8e385fde77a36ccd625f702c6b74e4
akmitrich <ak.mitrich@mail.ru>
2022-11-11 16:48:40 +0300

    Senior

--- examples/test_primes.rs
+++ examples/test_primes.rs
@@ -1,3 +1,53 @@
+use otus_algo_3::tester;
+
 fn main() {
-    dbg!(otus_algo_3::prime::linear_sieve(10));
+    println!("{}", include_str!("5.Primes/problem.txt"));
+    println!("Solve with simple search.");
+    tester::run_test_lim(
+        "examples/5.Primes", 
+        |data| {
+            let n = data.first().unwrap().parse::<u64>().unwrap();
+            let number_of_primes = otus_algo_3::prime::count_primes_naive(n);
+            number_of_primes.to_string()
+        }, 
+        9,
+    );
+    println!("\nSolve with optimized search.");
+    tester::run_test_lim(
+        "examples/5.Primes", 
+        |data| {
+            let n = data.first().unwrap().parse::<u64>().unwrap();
+            let number_of_primes = otus_algo_3::prime::get_prime_count(n);
+            number_of_primes.to_string()
+        }, 
+        13,
+    );
+    println!("\nSolve with optimized memory Erathos sieve.");
+    tester::run_test(
+        "examples/5.Primes",
+        |data| {
+            let n = data.first().unwrap().parse::<usize>().unwrap();
+            let number_of_primes = otus_algo_3::prime::effective_sieve(n);
+            number_of_primes.to_string()
+        }
+    );
+    println!("\nSolve with Erathos sieve.");
+    tester::run_test(
+        "examples/5.Primes",
+        |data| {
+            let n = data.first().unwrap().parse::<usize>().unwrap();
+            let number_of_primes = otus_algo_3::prime::prime_sieve(n);
+            number_of_primes.to_string()
+        }
+    );
+    println!("\nSolve with linear sieve algo O(N).");
+    tester::run_test_lim(
+        "examples/5.Primes", 
+        |data| {
+            let n = data.first().unwrap().parse::<usize>().unwrap();
+            let number_of_primes = otus_algo_3::prime::linear_sieve(n);
+            number_of_primes.to_string()
+        },
+        14, 
+    );
 }
--- src/prime.rs
+++ src/prime.rs
@@ -70,19 +70,50 @@ fn expunge(p: usize, sieve: &mut[bool]) {
 }
 
 pub fn linear_sieve(n: usize) -> usize {
-    let mut pr = vec![];
-    let mut lp = vec![0; n + 1];
+    let mut primes = vec![];
+    let mut sieve = vec![0; n];
     for i in 2..=n {
-        if lp[i] == 0 {
-            lp[i] = i;
-            pr.push(i);
+        if sieve[i - 1] == 0 {
+            sieve[i - 1] = i;
+            primes.push(i);
         }
-        for p in pr.iter() {
-            if p > &lp[i] || p * i > n { break; }
-            lp[p * i] = *p;
+        for p in primes.iter() {
+            if p > &sieve[i - 1] || p * i > n { break; }
+            sieve[p * i - 1] = *p;
         }
     }
-    pr.len()
+    primes.len()
+}
+
+pub fn effective_sieve(n: usize) -> usize {
+    const INNER_SIZE: usize = 32;
+    let storage_size = n / INNER_SIZE + 1;
+    let mut storage: Vec<u32> = vec![!0; storage_size];
+    storage[0] &= !1;
+    for p in 2..n {
+        let storage_index = (p - 1) / INNER_SIZE;
+        let bit_index = (p - 1) % INNER_SIZE;
+        let store = storage[storage_index];
+        if (store & (1 << bit_index)) > 0 {
+            let mut mult = p + p;
+            while mult <= n {
+                let storage_index = (mult - 1) / INNER_SIZE;
+                let bit_index = (mult - 1) % INNER_SIZE;
+                storage[storage_index] &= !(1 << bit_index);
+                mult += p;
+            }
+        }
+    }
+    let mut sum = 0;
+    for i in 0..n {
+        let storage_index = i / INNER_SIZE;
+        let bit_index = i % INNER_SIZE;
+        let store = storage[storage_index];
+        if (store & (1 << bit_index)) > 0 {
+            sum += 1;
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -92,6 +123,11 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(4, count_primes_naive(10));
+        assert_eq!(25, dbg!(count_primes_naive(100)));
+        assert_eq!(168, dbg!(count_primes_naive(1000)));
+        assert_eq!(1229, dbg!(count_primes_naive(10_000)));
+        assert_eq!(9592, dbg!(count_primes_naive(100_000)));
+        assert_eq!(78_498, count_primes_naive(1_000_000));
     }
 
     #[test]
@@ -103,6 +139,7 @@ mod tests {
         assert_eq!(9592, get_prime_count(100_000));
         assert_eq!(78_498, get_prime_count(1_000_000));
         assert_eq!(664_579, get_prime_count(10_000_000));
+        assert_eq!(5_761_455, get_prime_count(100_000_000));
     }
 
     #[test]
@@ -116,6 +153,19 @@ mod tests {
         assert_eq!(664_579, prime_sieve(10_000_000));
         assert_eq!(5_761_455, prime_sieve(100_000_000));
         assert_eq!(50_847_534, prime_sieve(1_000_000_000));
+    }
+
+    #[test]
+    fn test_effective_sieve() {
+        assert_eq!(4, effective_sieve(10));
+        assert_eq!(25, effective_sieve(100));
+        assert_eq!(168, effective_sieve(1000));
+        assert_eq!(1229, effective_sieve(10_000));
+        assert_eq!(9592, effective_sieve(100_000));
+        assert_eq!(78_498, effective_sieve(1_000_000));
+        assert_eq!(664_579, effective_sieve(10_000_000));
+        assert_eq!(5_761_455, effective_sieve(100_000_000));
+        assert_eq!(50_847_534, effective_sieve(1_000_000_000));
     }
 
     #[test]
